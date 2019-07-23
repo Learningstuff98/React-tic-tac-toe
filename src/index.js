ae5cb17286500a9f67c49bbd2b7063caf7a80c56
Andy Strube <andystrube64@gmail.com>
2019-07-23 16:06:01 -0700

    Moved state to the game class

--- src/index.js
+++ src/index.js
@@ -4,19 +4,12 @@ import './index.css';
 
 class Square extends React.Component {
 
-    constructor(props) {
-      super(props);
-      this.state = {
-        value: null,
-      };
-    }
-
     render() {
       return (
         <button 
           className="square" 
-          onClick={() => this.setState({value: 'X'}) }>
-          {this.state.value}
+          onClick={() => this.props.onClick()}>
+          {this.props.value}
         </button>
       );
     }
@@ -31,13 +24,24 @@ class Square extends React.Component {
       };
     }
 
-    renderSquare(i) {
-      return <Square value={this.state.squares[i]} />;
+    handleClick(i) {
+      const squares = this.state.squares.slice();
+      squares[i]= 'X';
+      this.setState({squares: squares});
     }
-  
+
+    renderSquare(i) {
+      return (
+        <Square 
+          value={this.state.squares[i]}
+          onClick={() => this.handleClick(i)}
+        />
+      );
+    }
+
     render() {
       const status = 'Next player: X';
-  
+
       return (
         <div>
           <div className="status">{status}</div>
